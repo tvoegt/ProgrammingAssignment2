e0cc71762c77504697e59ee4a7be3d5cc11db3f5
tvoegt <twvoegt92@gmail.com>
2015-11-22 12:25:34 +0000

    Assignment 2 Code
    Caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+#Function for storing a cached matrix to memory 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ##When ,aking the Cache, set m = NULL 
+  m <- NULL
+  
+  #function set sets x = y and m = NULL in a differnet enviroment
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  #get funciton stores the Cache inverse and gives it back 
+  #to the user user if called
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#Fuction to solve the inverse of the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  
+  #Checks to see if m = Null
+  #if m = Null it returns the inverse and 
+  #stores the answer in Cache
+  #If m !NULL then return the Cached Matrix
+  if(!is.null(m)) {
+    
+    #Tells the User the answer is stored in Cache 
+    #and being returned
+    message("getting cached data")
+    return(m) #Return the Cached Matrix
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...) #use solve function to inverse the matrix
+  x$setinverse(m) #store the inverse matrix in Cache
+  
+  m #return the inversed Matrix
 }
